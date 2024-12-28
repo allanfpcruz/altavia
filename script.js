d537b2587fde265419cdcf37a0d947f6ab939d2a
Allan Felipe <allanfpcruz@outlook.com>
2024-12-27 22:26:08 -0300

    reset everything

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ document.addEventListener('DOMContentLoaded', () => {
     pagination: {
       el: '.swiper-pagination'
     },
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false
+    },
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev'
--- tailwind.js
+++ tailwind.js
@@ -5,6 +5,12 @@ tailwind.config = {
         sans: ['Poppins', 'Arial', 'sans-serif'],
         serif: ['Chonburi', 'serif']
       },
+      colors: {
+        beige: "#BFA980",
+        lightBeige: "#F3EAC9",
+        blue: "#4A5B63",
+        lightBlue: "#A9CDD4"
+      }
     }
   }
 }
