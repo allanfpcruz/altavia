b0def6ce11d6fb6e092e964c8455a56b78f26fba
Allan Felipe <allanfpcruz@outlook.com>
2024-12-29 17:57:26 -0300

    finished cards and started the parallax and progressbar

--- tailwind.js
+++ tailwind.js
@@ -9,7 +9,12 @@ tailwind.config = {
         beige: "#BFA980",
         lightBeige: "#F3EAC9",
         blue: "#4A5B63",
-        lightBlue: "#A9CDD4"
+        lightBlue: "#A9CDD4",
+        yellow: "#ffaa00",
+        red: "#ff0000",
+        geen: "#00ff00",
+        gray: "#cccccc",
+        darkGray: "#898989",
       },
       backgroundImage: {
         "header-gradient": "linear-gradient(to bottom, #ffffff, transparent)"
