3486fde23172f7d787a950c59ad84a7a6a56c9df
Allan Felipe <allanfpcruz@outlook.com>
2024-12-31 10:13:10 -0300

    inicio da progress bar

--- script.js
+++ script.js
@@ -10,10 +10,6 @@ document.addEventListener('DOMContentLoaded', () => {
       delay: 3000,
       disableOnInteraction: false
     },
-    // navigation: {
-    //   nextEl: '.swiper-button-next',
-    //   prevEl: '.swiper-button-prev'
-    // }
   })
   document.addEventListener('keydown', (e) => {
     if(e.key === 'ArrowLeft') {
@@ -21,74 +17,62 @@ document.addEventListener('DOMContentLoaded', () => {
     } else if (e.key === 'ArrowRight') {
       swiper.slideNext()
     }
-  })
-
-  //progressbar
-  // let containerA = document.querySelector('#circleA')
-
-  // let circleA = new ProgressBar.Circle(containerA, {
-  //   color: '#64daf9',
-  //   strokeWidth: 8,
-  //   duration: 1400,
-  //   from: {color: '#aaa'},
-  //   to: {color: '#64daf9'},
-  //   step: function(state, circle) {
-  //       circle.path.setAttribute('stroke', state.color)
-
-  //       let value = Math.round(circle.value() * 60)
-
-  //       circle.setText(value)
-  //   }
-  // })
-
-  // let containerB = document.querySelector('#circleB')
-
-  // let circleB = new ProgressBar.Circle(containerA, {
-  //   color: '#64daf9',
-  //   strokeWidth: 8,
-  //   duration: 1400,
-  //   from: {color: '#aaa'},
-  //   to: {color: '#64daf9'},
-  //   step: function(state, circle) {
-  //       circle.path.setAttribute('stroke', state.color)
-
-  //       let value = Math.round(circle.value() * 60)
-
-  //       circle.setText(value)
-  //   }
-  // })
-
-  // let containerC = document.querySelector('#circleC')
-
-  // let circleC = new ProgressBar.Circle(containerA, {
-  //   color: '#64daf9',
-  //   strokeWidth: 8,
-  //   duration: 1400,
-  //   from: {color: '#aaa'},
-  //   to: {color: '#64daf9'},
-  //   step: function(state, circle) {
-  //       circle.path.setAttribute('stroke', state.color)
-
-  //       let value = Math.round(circle.value() * 60)
-
-  //       circle.setText(value)
-  //   }
-  // })
-
-  // let containerD = document.querySelector('#circleD')
-
-  // let circleD = new ProgressBar.Circle(containerA, {
-  //   color: '#64daf9',
-  //   strokeWidth: 8,
-  //   duration: 1400,
-  //   from: {color: '#aaa'},
-  //   to: {color: '#64daf9'},
-  //   step: function(state, circle) {
-  //       circle.path.setAttribute('stroke', state.color)
-
-  //       let value = Math.round(circle.value() * 60)
-
-  //       circle.setText(value)
-  //   }
-  // })
+  })  
 })
+
+const totalLength = 390
+
+//círculo 1
+const progressCircle = document.querySelector('#circle-container-1 .progress-circle')
+const progressText = document.querySelector('#circle-container-1 .progress-text')
+const targetProgress = 78 //preencher totalmente o círculo
+
+const observer1 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if(entry.isIntersecting) {
+      let currentProgress = 0
+      const interval = setInterval(() => {
+        if(currentProgress <= targetProgress) {
+          const offset = (currentProgress * (totalLength / targetProgress)) / 100
+          progressCircle.style.strokeDashoffset = offset
+          progressText.textContent = `+${currentProgress}`
+          currentProgress++
+        } else {
+          clearInterval(interval)
+        }
+      }, 15)
+      observer1.unobserve(entry.target)
+    }
+  })
+}, { threshold: 0.5 })
+
+//aplica ao elemento 
+observer1.observe(document.querySelector('#circle-container-1'))
+
+
+//círculo 2
+const progressCircle2 = document.querySelector('#circle-container-2 .progress-circle')
+const progressText2 = document.querySelector('#circle-container-2 .progress-text')
+const targetProgress2 = 750 //preencher totalmente o círculo
+
+const observer2 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if(entry.isIntersecting) {
+      let currentProgress = 0
+      const interval = setInterval(() => {
+        if(currentProgress <= targetProgress2) {
+          const offset = (currentProgress * (totalLength / targetProgress2)) / 100
+          progressCircle2.style.strokeDashoffset = offset
+          progressText2.textContent = `+${currentProgress}`
+          currentProgress++
+        } else {
+          clearInterval(interval)
+        }
+      }, 5)
+      observer1.unobserve(entry.target)
+    }
+  })
+}, { threshold: 0.5 })
+
+//aplica ao elemento 
+observer2.observe(document.querySelector('#circle-container-2'))
